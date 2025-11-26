760a722287d263fa620607ee49e84abc7478a0bb
zeyonic <mike.andersson@hotmail.com>
2025-11-26 05:04:23 +0100

    finishing polish on 1.5
    started re-integrating kobolds - still not finished though
    raised the min pop of some subterranian creatures
    stabilized world population of the races, predominantly orcs/goblins/halflings
    made the dwarven castes mostly asexual so they're not so inclined to form lineages
    altered the human naming symbols so their kingsdoms are including words like empire, kingdom, dominion, etc
    disabled the rcp_injector until it's been refined
    fixed vampires and necromancer material force vulnerabilities
    made entity armor items forced so they always spawn with armor
    added a 1 tick recovery time on guns between each shot
    reworked human cultural entity values so they're more weighted towards certain values but not forced
    tweaked human nobles' personalities so they're weighted towards personalities that infer selfishness, greed, ambition etc
    
    misc plant tweaks
    
    re-enabled the subterranian fff plants - but still have the mudshroom drink disabled as it messes up prepare carefully embark drinks
    other misc things like de-capitalizing reaction descriptions

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,6 @@
 require("masterfork_rcp_tweaks")
 require("masterfork_rcp_worship")
-require("masterfork_rcp_injector")
+--require("masterfork_rcp_injector") disabled until it's been reworked
 require("masterfork_secret_necromancy")
 require("masterfork_curse_vampirism")
 require("masterfork_scripts")
