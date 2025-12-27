e1bce92e76a459e8f105a59c705fb45d31be0f83
zeyonic <mike.andersson@hotmail.com>
2025-12-27 18:03:07 +0100

    dev branch 1.7.2 initial commit
    cleaned up and updated some leftover creature sprites
    properly applied the new creature portraits from the base game
    fixed certain reactions improperly requiring extremely tiny amounts of metal (adv smelting and bulk arrows)
    higher tier metal smelting provide additional reagents (mithril and above)
    added two new armor items for civ use - the padded leather armor and helm which require more materials but protect more body parts (armor protect the hips and neck)
    increased the size of ribs by 15% (100 to 115) due to them breaking to easily
    reduced the already existent global learning penalty to 50% (down from 75%)
    reverted the unintended human skill nerf back to 75% on all skills (up from 50%)
    fixed some animal people attacks lacking prepare/recover tags
    standardized animal gore (horn/tusk) attacks and they should have better penetration in combat now
    increased the cooldown of creature body regenerative abilities to 24h (hydras, golems, etc.)
    fixed certain creature body attacks lacking proper attack verbs (kicking did not say kick/kicks)
    all (non-rcp) creatures had their kicking attacks reworked; now have two kicking attacks but with double cooldown. this is to increase the chance of creatures choosing kicking over scratching/biting in combat that had those as secondary priority
    reworked kicking attacks; kicks now have a 25% increase to attack velocity
    strengthened nails/claws and increased sharpness by 15%
    reworked animal attacks; hooved animals' kicks have a 50% velocity increase, but worse biting. predator/canine toothed animals have sharper teeth. clawed animals have sharper claws.
    reworked human civ; now split into two entities; each have opposing ethics and cultural value weights with the empire valuing war, etiquette, and order, while the confederation valuing the opposite with an emphasis on peace and trade. note: cultural values are still mostly randomized but are weighted depending on the civ, for example the empire have peace weighted "-33:23", meaning an average value of -5, while the confederation average is +5
    misc word tweaks
    renamed the "brew x of dwarven wine" reaction(s) to "brew x of dwarven whiskey"
    updated horn beetles' horn length/broadness to be properly described in in the creature description menu
    updated world_gen.txt
    updated masterfork_add_regular_tokens.lua

--- scripts/masterfork_add_regular_tokens.lua
+++ scripts/masterfork_add_regular_tokens.lua
@@ -3,6 +3,7 @@ old_add_regular_tokens=add_regular_tokens
 function add_regular_tokens(lines,options)
 	old_add_regular_tokens(lines,options)
 	lines[#lines+1]="[APPLY_CREATURE_VARIATION:MEGABEAST_FORCE_RESISTANCE]"-- very high resistance against non-metal weapons
+	lines[#lines+1]="[APPLY_CURRENT_CREATURE_VARIATION]"-- just to be safe?
 	lines[#lines+1]="[STANCE_CLIMBER]"-- so rcp creatures can climb
 	lines[#lines+1]="[NATURAL_SKILL:CLIMBING:1]"-- nat climbing skill
 end
